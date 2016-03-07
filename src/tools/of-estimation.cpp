764530cf9b4bacea90087c11dd9242f021839632
Douglas Uba <dougmessias@gmail.com>
2016-03-07 16:11:49 -0300

    Changed to process a list of consecutive images

--- src/tools/of-estimation.cpp
+++ src/tools/of-estimation.cpp
@@ -17,70 +17,109 @@
 
 // STL
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
+
+void Tokenize(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiter)
+{
+  std::size_t lastpos = str.find_first_not_of(delimiter, 0);
+
+  std::size_t pos = str.find_first_of(delimiter, lastpos);
+
+  while(std::string::npos != pos || std::string::npos != lastpos)
+  {
+    tokens.push_back(str.substr(lastpos, pos - lastpos));
+    lastpos = str.find_first_not_of(delimiter, pos);
+    pos = str.find_first_of(delimiter, lastpos);
+  }
+}
+
+std::string Convert2String(std::size_t i)
+{
+  std::ostringstream strs; strs << i;
+  return strs.str();
+}
 
 int main(int argc, char** argv)
 {
   try
   {
     // Create the object that handles the input arguments
-    TCLAP::CmdLine cmd("A tool to estimate optical flow given two consecutive images", ' ', "1.0.0");
+    TCLAP::CmdLine cmd("A tool to estimate optical flow given a set of consecutive images", ' ', "1.0.0");
 
-    // Define image A path argument
-    TCLAP::ValueArg<std::string> aPathArg("a", "imagea", "The path of the first image (A)", true, "", "string");
+    // Define images path argument
+    TCLAP::ValueArg<std::string> imagesPathArg("i", "images", "Paths of consecutive images comma separated. e.g. path-1,path-2,path-n", true, "", "string");
 
-    // Define image B path argument
-    TCLAP::ValueArg<std::string> bPathArg("b", "imageb", "The path of the second image (B)", true, "", "string");
+    // Define output directory argument
+    TCLAP::ValueArg<std::string> outputDirArg("o", "output", "Path to output directory that will contain the results. \
+                                                              Each output file (.flo) contains the coordinates of flow vectors at instant t+1",
+                                                              true, "", "string");
 
-    // Define output file path for (.flo) result file
-    TCLAP::ValueArg<std::string> outputArg("o", "output", "Path to output file that will be created. The output file contains the coordinates of flow vectors", true, "", "string");
-
-    // Add the arguments (keeping the best order for the user)
-    cmd.add(outputArg);
-    cmd.add(bPathArg);
-    cmd.add(aPathArg);
+    // Add the arguments
+    cmd.add(outputDirArg);
+    cmd.add(imagesPathArg);
 
     // Parse the given input parameters from agv array
     cmd.parse(argc, argv);
+
+    // Get the set of image paths separately
+    std::vector<std::string> paths;
+    Tokenize(imagesPathArg.getValue(), paths, ",");
+
+    if(paths.size() < 2)
+      throw of::Exception("Wrong parameter 'images': inform at least two consecutive images");
 
     // Everything is ok! Let's run the process
 
     // GDAL initialization
     GDALAllRegister();
 
-    // Open input images
-    GDALDataset* gDatasetA = (GDALDataset*)GDALOpen(aPathArg.getValue().c_str(), GA_ReadOnly);
-    GDALDataset* gDatasetB = (GDALDataset*)GDALOpen(bPathArg.getValue().c_str(), GA_ReadOnly);
+    std::cout << "Processing..." << std::endl;
 
-    // Retrieve dimensions
-    std::size_t nlines = gDatasetA->GetRasterYSize();
-    std::size_t ncols = gDatasetA->GetRasterXSize();
-    std::size_t size = nlines * ncols;
+    for(std::size_t i = 0; i < paths.size() - 1; ++i) // for each image pair
+    {
+      std::cout << "- Image A: " << paths[i] << std::endl;
+      std::cout << "- Image B: " << paths[i + 1] << std::endl;
 
-    // Read buffers
-    double* buffera = new double[size];
-    double* bufferb = new double[size];
-    gDatasetA->GetRasterBand(1)->RasterIO(GF_Read, 0, 0, ncols, nlines, buffera, ncols, nlines, GDT_Float64, 0, 0);
-    gDatasetB->GetRasterBand(1)->RasterIO(GF_Read, 0, 0, ncols, nlines, bufferb, ncols, nlines, GDT_Float64, 0, 0);
+      // Open input images
+      GDALDataset* gDatasetA = (GDALDataset*)GDALOpen(paths[i].c_str(), GA_ReadOnly);
+      GDALDataset* gDatasetB = (GDALDataset*)GDALOpen(paths[i + 1].c_str(), GA_ReadOnly);
 
-    // Close! GDAL is used only to read image data
-    GDALClose(gDatasetA);
-    GDALClose(gDatasetB);
+      // Retrieve dimensions
+      std::size_t nlines = gDatasetA->GetRasterYSize();
+      std::size_t ncols = gDatasetA->GetRasterXSize();
+      std::size_t size = nlines * ncols;
 
-    // Encapsulate buffers
-    of::Image* imga = new of::Image(buffera, nlines, ncols);
-    of::Image* imgb = new of::Image(bufferb, nlines, ncols);
+      // Read buffers
+      double* buffera = new double[size];
+      double* bufferb = new double[size];
+      gDatasetA->GetRasterBand(1)->RasterIO(GF_Read, 0, 0, ncols, nlines, buffera, ncols, nlines, GDT_Float64, 0, 0);
+      gDatasetB->GetRasterBand(1)->RasterIO(GF_Read, 0, 0, ncols, nlines, bufferb, ncols, nlines, GDT_Float64, 0, 0);
 
-    // Execute!
-    of::OpticalFlow* of = new of::LucasKanadeC2F(imga, imgb);
-    of->compute();
+      // Close! GDAL is used only to read image data
+      GDALClose(gDatasetA);
+      GDALClose(gDatasetB);
 
-    // Save result as (.flo) file
-    of->save(outputArg.getValue());
+      // Encapsulate buffers
+      of::Image* imga = new of::Image(buffera, nlines, ncols);
+      of::Image* imgb = new of::Image(bufferb, nlines, ncols);
 
-    delete of;
-    delete imga;
-    delete imgb;
+      // Execute!
+      of::OpticalFlow* of = new of::LucasKanadeC2F(imga, imgb);
+      of->compute();
+
+      std::string uvfile = outputDirArg.getValue() + "uv-" + Convert2String(i + 1) + ".flo";
+
+      std::cout << "- Result file (u,v): " << uvfile << std::endl;
+
+      // Save result as (.flo) file
+      of->save(uvfile);
+
+      delete of;
+      delete imga;
+      delete imgb;
+    }
   }
   catch(TCLAP::ArgException& e)
   {
